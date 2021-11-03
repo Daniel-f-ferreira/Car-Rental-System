070c400325aab0d7c2aa45633cd3039991fb53ad
Eric Mabe <73889661+erickiimm@users.noreply.github.com>
2021-11-03 20:16:19 +0000

    Booking System

--- BookingSystem.java
+++ BookingSystem.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ooc.yoursolution;
 
-/**
- *
- * @author Daniel
- */
-public class BookingSystem {
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class BookingSystem implements BookingSystemInterface {
+    
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+        String input;
+        String[] parameters;
+        int numberOfCars = 0;
+        List<CarInterface> cars;
+    
+        while ((input = in.readLine()) != null) {
+            parameters = input.split(":");
+            Car newCar = new Car();
+            newCar.setMake(Make.valueOf(parameters[0]);
+            // provavelmente precisa converter a rate pra double
+            newCar.setRate(parameters[1]);
+            cars.add(newCar);
+            numberOfCars += parameters[2];
+        }
+
+        RentACarInterface rentACar = new RentACar();
+        rentACar.setCars(cars);
+        rentACar.setNumberOfCars(numberOfCars);
+
+        return rentACar;
+    }
     
 }
